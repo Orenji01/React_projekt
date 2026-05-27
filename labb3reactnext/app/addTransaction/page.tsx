4e63377ed85e3d2a7c878a8d73ada3513ea481df
Madchili <jonas_nl@live.se>
2026-05-27 12:35:10 +0200

    added routes

--- labb3reactnext/app/addTransaction/page.tsx
+++ labb3reactnext/app/addTransaction/page.tsx
@@ -1,3 +1,29 @@
-export default function addTransaction({}) {
-	return <div>Bästa sidan ever</div>;
+"use client";
+import { useEffect, useState } from "react";
+
+interface responseData {
+	ok: boolean;
+	message: string;
+}
+
+export default function AddTransaction({}) {
+	const [data, setData] = useState<responseData | null>(null);
+
+	async function getData() {
+		const response = await fetch("/api/public/save_post");
+		const res: responseData = await response.json();
+		setData(res);
+		console.log(res.message);
+	}
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	return (
+		<div>
+			Bästa sidan ever
+			{<p>{data && data.message}</p>}
+		</div>
+	);
 }
