a7cc71ddf00dcde8e60b5990f3efa0ac355ed77b
LACRIM94 <anasmestour@gmail.com>
2026-08-21 08:52:51 +0200

    Ux and accesibilty changes

--- labb3reactnext/app/addSavegoal/page.tsx
+++ labb3reactnext/app/addSavegoal/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import styles from "./page.module.css";
 
 type Inputs = {
@@ -17,7 +17,6 @@ export default function Savegoal() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
     reset,
   } = useForm<Inputs>();
@@ -43,76 +42,133 @@ export default function Savegoal() {
   };
 
   return (
-    <div id={styles.container}>
-      <form id={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <h1 id={styles.title}>Nytt sparmål</h1>
-        <div className={styles.formSection}>
-          <label>Namn</label>
-          <input
-            className={styles.input}
-            type="text"
-            placeholder="namn"
-            {...register("name", { required: true })}
-          />
-        </div>
-        <div className={styles.formSection}>
-          <label>Summa</label>
-          <input
-            className={styles.input}
-            type="number"
-            placeholder="summa"
-            {...register("amount", { required: true })}
-          />
-        </div>
-        <div className={styles.formSection}>
-          <label>Månadsbetalning</label>
-          <input
-            className={styles.input}
-            type="number"
-            placeholder="månadsbetalning"
-            {...register("perMonth", { required: true })}
-          />
-        </div>
-        <div className={styles.formSection}>
-          <label>Ränta</label>
-          <input
-            className={styles.input}
-            type="number"
-            placeholder="ränta"
-            step="0.01"
-            {...register("interest", { required: true })}
-          />
-        </div>
-        <div className={styles.formSection}>
-          <label>Inflation</label>
-          <input
-            className={styles.input}
-            type="number"
-            placeholder="inflation"
-            step="0.01"
-            {...register("inflation", { required: true })}
-          />
-        </div>
-        <div className={styles.formSection}>
-          <label>Start-datum</label>
-          <input
-            className={styles.input}
-            type="date"
-            placeholder="start-datum"
-            {...register("startDate", { required: true })}
-          />
-        </div>
-        <div className={styles.formSection}>
-          <label>Mål-datum</label>
-          <input
-            className={styles.input}
-            type="date"
-            placeholder="mål-datum"
-            {...register("targetDate", { required: true })}
-          />
-        </div>
-        <input id={styles.button} type="submit" value="Lägg till" />
-      </form>
-    </div>
+    <main>
+      <div id={styles.container}>
+        <form id={styles.form} onSubmit={handleSubmit(onSubmit)}>
+          <h1 id={styles.title}>Nytt sparmål</h1>
+          <div className={styles.formSection}>
+            <label htmlFor="name">Namn</label>
+            <input
+              className={styles.input}
+              type="text"
+              id="name"
+              {...register("name", { required: true })}
+              aria-invalid={errors.name ? "true" : "false"}
+              aria-describedby={errors.name ? "name-error" : undefined}
+            />
+          </div>
+          {errors.name && (
+            <span id="name-error" className={styles.errorMessage}>
+              Namn saknas
+            </span>
+          )}
+          <div className={styles.formSection}>
+            <label htmlFor="amount">Summa</label>
+            <input
+              className={styles.input}
+              type="number"
+              id="amount"
+              {...register("amount", { required: true })}
+              aria-invalid={errors.amount ? "true" : "false"}
+              aria-describedby={errors.amount ? "amount-error" : undefined}
+            />
+          </div>
+          {errors.amount && (
+            <span id="amount-error" className={styles.errorMessage}>
+              Summa saknas
+            </span>
+          )}
+          <div className={styles.formSection}>
+            <label htmlFor="per-month">Månadsbetalning</label>
+            <input
+              className={styles.input}
+              type="number"
+              id="per-month"
+              {...register("perMonth", { required: true })}
+              aria-invalid={errors.perMonth ? "true" : "false"}
+              aria-describedby={errors.perMonth ? "permonth-error" : undefined}
+            />
+          </div>
+          {errors.perMonth && (
+            <span id="permonth-error" className={styles.errorMessage}>
+              Månadsbetalning saknas
+            </span>
+          )}
+          <div className={styles.formSection}>
+            <label htmlFor="interest">Ränta</label>
+            <input
+              className={styles.input}
+              type="number"
+              id="interest"
+              step="0.01"
+              {...register("interest", { required: true })}
+              aria-invalid={errors.interest ? "true" : "false"}
+              aria-describedby={errors.interest ? "interest-error" : undefined}
+            />
+          </div>
+          {errors.interest && (
+            <span id="interest-error" className={styles.errorMessage}>
+              Ränta måste vara minst 0
+            </span>
+          )}
+          <div className={styles.formSection}>
+            <label htmlFor="inflation">Inflation</label>
+            <input
+              className={styles.input}
+              type="number"
+              id="inflation"
+              step="0.01"
+              {...register("inflation", { required: true })}
+              aria-invalid={errors.inflation ? "true" : "false"}
+              aria-describedby={
+                errors.inflation ? "inflation-error" : undefined
+              }
+            />
+          </div>
+          {errors.inflation && (
+            <span id="inflation-error" className={styles.errorMessage}>
+              Inflation måste vara minst 0
+            </span>
+          )}
+          <div className={styles.formSection}>
+            <label htmlFor="start-datum">Start-datum</label>
+            <input
+              className={styles.input}
+              type="date"
+              id="start-datum"
+              {...register("startDate", { required: true })}
+              aria-invalid={errors.startDate ? "true" : "false"}
+              aria-describedby={
+                errors.startDate ? "startdate-error" : undefined
+              }
+            />
+          </div>
+          {errors.startDate && (
+            <span id="startdate-error" className={styles.errorMessage}>
+              Startdatum saknas
+            </span>
+          )}
+          <div className={styles.formSection}>
+            <label htmlFor="mål-datum">Mål-datum</label>
+            <input
+              className={styles.input}
+              type="date"
+              id="mål-datum"
+              {...register("targetDate", { required: true })}
+              aria-invalid={errors.targetDate ? "true" : "false"}
+              aria-describedby={
+                errors.targetDate ? "targetdate-error" : undefined
+              }
+            />
+          </div>
+          {errors.targetDate && (
+            <span id="targetdate-error" className={styles.errorMessage}>
+              Måldatum saknas
+            </span>
+          )}
+          <input id={styles.button} type="submit" value="Lägg till" />
+        </form>
+      </div>
+    </main>
   );
 }
